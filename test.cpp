4c086bc2aa73009a01e79a2557c95ae7a80b82c5
Rhithick02 <rhithick02@gmail.com>
2020-04-27 19:24:52 +0530

    Update

--- test.cpp
+++ test.cpp
@@ -1,14 +1,28 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
+int pickingNumbers(vector<int> ar) 
+{
+    set<int> st,store;
+    int arr[100]={0};
+    for(int i=0;i<ar.size();i++)
+        st.insert(ar[i]);
+    for(int i:st)
+    {
+        arr[i]=count(ar.begin(),ar.end(),i);
+    }
+    for(int i=1;i<100;i++)
+    {
+        store.insert(arr[i]+arr[i-1]);
+    }
+    return *store.rbegin();
+}
 int main()
 {
-    string st1,st2;
-    cin>>st1>>st2;
-    st1[0]++;
-    st2+='h'*'h';
-    //cout<<st1[0] -st2[0]-1;
-//    cout<<(st1[1]-'1');
-    cout<<st2;
+    int n;
+    cin>>n;    
+    vector<int>ar(n);
+    for(int i=0;i<n;i++)
+        cin>>ar[i];
+    cout<<pickingNumbers(ar);
     return 0;
-    
 }
