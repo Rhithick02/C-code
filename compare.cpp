551113ccfaeaa336e5a6cae7e7827520f8aba8e0
Rhithick02 <rhithick02@gmail.com>
2020-05-02 13:02:28 +0530

    Update

--- compare.cpp
+++ compare.cpp
@@ -6,24 +6,7 @@ for(int k=3;k<=100;k++)
 {
     vector<bool>ar(1000+1);
     bool temp;
-	ar[0]=0,ar[1]=1,ar[2]=1;
-    for(int i=3;i<=1000;i++)
-	{
-		if(i<k)
-		{
-            if(!ar[i-1] || !ar[i-2])
-                ar[i]=1;
-            else 
-                ar[i]=0;
-        }
-        else 
-        {
-            if(!ar[i-1] || !ar[i-2] || !ar[i-k])
-                ar[i]=1;
-            else 
-                ar[i]=0;
-        }
-    }
+	
     for(int n=2;n<=1000;n++)
     {
         if(n==k)
--- mathproblem.cpp
+++ mathproblem.cpp
@@ -18,28 +18,37 @@ void primefactors(int n,int prime[])
 int main(){
     int t;
     cin>>t;
+
+    vector< bool > wh(50001,true);
+    wh[1]=0;
+    for(int i=2;i<=50000;i++)
+        if(wh[i])
+            for(int j=2*i;j<=50000;j+=i)
+                wh[j]=false;
+
     while(t--)
     {
         int n;
         lli prod=1;
         cin>>n;
-        if(n==0)
-            cout<<"1\n";
-        else if(n==1)
-            cout<<"1\n";
+        if(n==0 || n==1)
+            cout<<"1";
         else        
         {
-            int prime[n+1]={0};
-            for(lli i=2;i<=n;i++)
-                primefactors(i,prime);
-            for(lli i=2;i<=n;i++)
+            vector< int> pri(50000, 0);
+            for(int i=2;i<=n;i++)
             {
-                if(prime[i]!=0)
-                    prod*=(prime[i]+1);
-                prod=prod%mod;
+                if(!wh[i])
+                    continue;
+                int te=n;
+                while(te)
+                    pri[i]+=(te/i), te/=i;
             }
-            cout<<prod%mod<<endl;
+            for(int i=2;i<=n;i++)
+                prod*=(pri[i]+1),prod%=mod;
+            cout<<prod;
         }
+        cout<<endl;
     }
     return 0;
 }
