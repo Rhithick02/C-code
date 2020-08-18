46199b757653d6b2a76dbac4f864579739c2d7e2
Alchemist <rhithick02@gmail.com>
2020-08-18 17:30:24 +0530

    Update

--- Codeforces/1059C.cpp
+++ Codeforces/1059C.cpp
@@ -13,18 +13,25 @@ using namespace std;
 
 int main(){
     ios::sync_with_stdio(false);
-    int n;
+    int n, maxi = 0, val, fl = 0;
     cin >> n;
     vector <int> st(21);
+    maxi = log2(n);
     For(i, 1, n+1) {
-        int cnt = 0, te = i;;
-        while(te % 2 == 0) {
-            cnt++;
-            te /= 2;
-        }
+        int cnt = 0, te = i;
+        while(te % 2 == 0)
+            cnt++, te /= 2;
         st[cnt]++;
+        if(cnt == maxi - 1 && (1 << cnt != i)) {
+            if(!fl) {
+                st[maxi]--;
+                fl = 1;
+            }
+            val = i;
+        }
     }
     For(i, 0, 21)
         For(j, 0, st[i])
             cout << (1 << i) << " ";
+    if(fl) cout << val << " ";
 }
