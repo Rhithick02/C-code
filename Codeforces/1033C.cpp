ca206c07378745140d21d82833ae834bdd41c2db
Alchemist <rhithick02@gmail.com>
2020-08-22 23:08:23 +0530

    Update

--- Codeforces/1033C.cpp
+++ Codeforces/1033C.cpp
@@ -15,5 +15,19 @@ int main(){
     ios::sync_with_stdio(false);
     int n;
     cin >> n;
-    
+    vector <int> st[n + 1], ar(n), res(n + 1);
+    For(i, 0, n) cin >> ar[i];
+    For(i, 0, n) {
+        for(int j = i - ar[i]; j >=0; j -= ar[i])
+            if(ar[j] > ar[i]) st[ar[i]].pb(ar[j]);
+        for(int j = i + ar[i]; j < n; j += ar[i])
+            if(ar[j] > ar[i]) st[ar[i]].pb(ar[j]);
+    }
+    For(i, 0, n) {
+        int fl = -1;
+        for(auto it: st[n-i]) if(res[it] == -1) fl = 1;
+        res[n-i] = fl;
+    }
+    For(i, 0, n) cout << (res[ar[i]] == 1 ? "A":"B");
+    cout << endl;
 }
