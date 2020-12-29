1af3c43c4f81199d81c059d9b00fadba43974998
Alchemist <rhithick02@gmail.com>
2020-12-29 12:22:07 +0530

    Corrected cmax05.cpp

--- Codechef/CMAX2020/cmax05.cpp
+++ Codechef/CMAX2020/cmax05.cpp
@@ -17,44 +17,19 @@ using namespace std;
 typedef tree<int,null_type,less<int>,rb_tree_tag,
 tree_order_statistics_node_update> indexed_set;
 
-vector <lli> phi(MAX), pref(MAX);
+vector <lli> pref(MAX);
 
 int main(){
     ios::sync_with_stdio(false);
-    For(i, 0, MAX) phi[i] = i;
+    pref[1] = 1;
     For(i, 2, MAX) {
-        if(phi[i] == i) {
-            for(int j = i; j < MAX; j += i) {
-                phi[j] -= phi[j] / i;
-            }
-        }
+        pref[i] = (pref[i-1] + 1ll * i * (i+1) / 2) % mod;
     }
-    For(i, 1, MAX) pref[i] = (pref[i-1] + phi[i]) % mod;
     int t;
     cin >> t;
     while(t--) {
-        lli sum = 0;
         int n;
         cin >> n;
-        For(i, 1, n+1) {
-            vector <pair<int, int>> st;
-            for(int j = 0; ; j = i / (i / (j+1))) {
-                int next = i / (i / (j+1));
-                st.pb({next, i / (j+1)});
-                if(next == i) break;
-            }
-            For(j, 0, st.size()) {
-                if(!j) {
-                    sum += st[0].fi * st[0].se * pref[1];
-                } else {
-                    int dif = st[j].fi-st[j-1].fi;
-                    int toti = pref[st[j].fi]-pref[st[j-1].fi];
-                    sum += 1ll * dif * st[j].se * toti;
-                }
-                sum %= mod;
-            }
-
-        }
-        cout << sum << "\n";
+        cout << pref[n] << "\n";
     }
 }
