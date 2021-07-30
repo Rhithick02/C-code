f396b1207cc740abb0a5e77d08d30aeae0b984ff
Rhithick02 <rhithick02@gmail.com>
2021-07-30 16:55:50 +0530

    Update

--- Misc/dp6.cpp
+++ Misc/dp6.cpp
@@ -15,15 +15,15 @@ typedef tree<int,null_type,less<int>,rb_tree_tag,
 tree_order_statistics_node_update> indexed_set;
 
 int main() {
-    string s = "021";
-    cout << char('0' + 1) << endl;
-    set <int> te;
-    for(int i = 0; i < 5; i++)
-        te.insert(i+1);
-    auto it = te.begin();
-    auto ed = te.lower_bound(3);
-    for(; it != ed; it++) {
-        cout << *it << " ";
-        te.erase(it);
+    int n;
+    cin >> n;
+    vector <int> ar(n), dp(n, mod);
+    for(auto &it : ar) cin >> it;
+    dp[0] = 0;
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = i + 1; j <= min(n-1, i + ar[i]); j++) {
+            dp[j] = min(dp[j], dp[i] + 1);
+        }
     }
+    cout << dp[n-1] << endl;
 }
